695486df6faa94bf021a7047d44a0618ec2b01c4
pugongyingtear <1774897352@qq.com>
2017-08-23 21:44:15 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+// import Hello from '@/components/Hello'
 import index from './../components/index'
-import registration from './../components/registration'
-import login from './../components/login'
+// import registration from './../components/registration'
+// import login from './../components/login'
 // import Search from './../components/search'
-import takeOut from './../components/take-out'
+// import takeOut from './../components/take-out'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     { path: '/', name: 'index', component: index },
-    { path: '/registration', name: 'registration', component: registration},
-    { path: '/login', name: 'login', component: login},
+    // { path: '/registration', name: 'registration', component: registration},
+    // { path: '/login', name: 'login', component: login},
     // { path: '/search', name: 'search', component: Search},
-    { path: '/takeOut', name: 'take-out', component: takeOut}
+    // { path: '/takeOut', name: 'take-out', component: takeOut}
   ]
 })
